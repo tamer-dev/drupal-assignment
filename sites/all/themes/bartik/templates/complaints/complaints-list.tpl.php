ed17c3ee5b14a4a1c6eef8278f4584e49d57325f
tamer-dev <tamer.dev.alex@gmail.com>
2022-12-29 02:59:16 +0300

    fix issues

--- sites/all/themes/bartik/templates/complaints/complaints-list.tpl.php
+++ sites/all/themes/bartik/templates/complaints/complaints-list.tpl.php
@@ -3,25 +3,21 @@
 if(count($data) > 0)
 {
     echo "<table><tr>
-            <th>Id</th>
             <th>Complaint Name</th>
             <th>Email</th>
             <th>phone</th>
             <th>Complaint Type</th>
             <th>Status</th>
             </tr>";
-    $count = 1;
     foreach($data as $v)
     {
         echo "<tr>";
-        echo "<td>" . $count . "</td>";
-        echo "<td>" . $v->complaint_name['und'][0]['value'] . "</td>";
-        echo "<td>" . $v->email['und'][0]['value'] . "</td>";
-        echo "<td>" . $v->phone ['und'][0]['value']. "</td>";
-        echo "<td>" . $v->complaint_type['und'][0]['value'] . "</td>";
-        echo "<td>" . $v->workflow_status['und'][0]['value'] . "</td>";
+        if($v->complaint_name) echo "<td>" .  $v->complaint_name['und'][0]['value']  . "</td>"; else echo '<td></td>';
+        if($v->email) echo "<td>" . $v->email['und'][0]['value'] . "</td>";else echo '<td></td>';
+        if($v->phone) echo "<td>" . $v->phone ['und'][0]['value']. "</td>";else echo '<td></td>';
+        if($v->complaint_type) echo "<td>" . $v->complaint_type['und'][0]['value'] . "</td>";else echo '<td></td>';
+        if($v->workflow_status) echo "<td>" . $v->workflow_status['und'][0]['value'] . "</td>";else echo '<td></td>';
         echo "</tr>";
-        $count++;
     }
     echo "</table>";    
 }else{
